9f7922398a9a15fdd83b62bf416aa121bbcc2032
Fariha <farihaarifin@gmail.com>
2021-03-17 13:42:21 +0600

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Database config variable
+//echo"go go";
+define("DB_HOST","localhost");
+define("DB_USER","fariha_curd");
+define("DB_PASSWORD","simpleCURD");
+define("DB_DATABASE","simple_curd_system");
+
+?>
